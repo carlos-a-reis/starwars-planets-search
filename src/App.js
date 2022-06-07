729e31ccc671b9d7e672c091d44fa698d5295229
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-06-07 17:31:15 -0300

    Criaçao do helper de requisiçao a API, armazenamento dos dados da API no estado do App, estado do App  passado por context, criaçao e preenchimento da tabela com dados da API (requisito 1)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Context from './context/Context';
+import Table from './Components/Table';
+import fetchPlanets from './Helpers/FetchPlanets';
 
 function App() {
+  const [value, setValue] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchPlanets();
+      setValue({ data });
+    };
+    getData();
+  }, []);
+
   return (
-    <Context.Provider>
-      <span>Hello!</span>
+    <Context.Provider value={ value }>
+      <Table />
     </Context.Provider>
   );
 }
