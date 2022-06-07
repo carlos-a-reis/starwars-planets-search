df574e5a233f18837dc19dd411173c97a310d0be
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-06-07 16:33:34 -0300

    Preparaçao da aplicaçao para o uso do Context API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Context from './context/Context';
 
 function App() {
   return (
-    <span>Hello, App</span>
+    <Context.Provider>
+      <span>Hello!</span>
+    </Context.Provider>
   );
 }
 
