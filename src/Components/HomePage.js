592d7b026a2d878f9ed71c638ae7e88750d16d25
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-07-19 18:58:30 -0300

    Adição da logo de star wars na página inicial do projeto

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import arrow from '../images/arrow.png';
 import '../CSS/homePage.css';
+import logo from '../images/star-wars.png';
 
 function HomePage() {
   return (
     <div className="home-page" id="home-page">
-      <h1 className="home-img">HOME</h1>
+      <img src={ logo } alt="gif com a logo de Star Wars" className="home-img" />
 
       { /* link para a parte da tabela da página */ }
       <a href="#planets-search" className="table-link">
