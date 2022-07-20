caa01c5b6dd60e44e9c1de06a88c2ba3d8ead9fd
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-07-20 18:57:55 -0300

    Criação da animação da página inicial da aplicação

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import arrow from '../images/arrow.png';
 import '../CSS/homePage.css';
-import logo from '../images/star-wars.png';
+import star from '../images/star.png';
+import wars from '../images/wars.png';
+import planets from '../images/planets-search.png';
 
 function HomePage() {
   return (
     <div className="home-page" id="home-page">
-      <img src={ logo } alt="gif com a logo de Star Wars" className="home-img" />
+      <img src={ star } alt="gif com a logo de Star Wars" className="star-img" />
+      <img src={ wars } alt="gif com a logo de Star Wars" className="wars-img" />
+      <img
+        src={ planets }
+        alt="gif com a logo de Star Wars"
+        className="planets-search-img"
+      />
 
       { /* link para a parte da tabela da página */ }
       <a href="#planets-search" className="table-link">
